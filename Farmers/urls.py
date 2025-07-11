8d92b248111916673dc9786c355ff59a97ae822f
SRIJAN_RAY <srijanray11@gmail.com>
2025-07-11 07:46:35 +0530

    farmer site update 20%

--- Farmers/urls.py
+++ Farmers/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('farmers/', Farmers, name='Farmers'),
-    path('farmers_logout/', Farmers_logout, name='farmers_logout'),
+    path("manage_items/", Manage_items, name="Manage_items"),
+    path('farmers_logout/', Farmers_logout, name='Farmers_logout'),
 ]
--- Farmers/views.py
+++ Farmers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from Auth.decorators import custom_login_required
-
+from .models import Item
+from django.contrib import messages
 # Create your views here.
 @custom_login_required
 def Farmers(request):
@@ -19,6 +20,42 @@ def Farmers(request):
     
     return render(request, 'Farmers/FarmerDashboard.html', {'user': user})
 
+@custom_login_required
+def Manage_items(request):
+    if request.method == "POST":
+        action = request.POST.get("action")
+        item_id = request.POST.get("id")
+
+        if action == "add":
+            Item.objects.create(
+                name=request.POST["name"],
+                quantity=request.POST["quantity"],
+                category=request.POST["category"],
+                price=request.POST["price"]
+            )
+            messages.success(request, "Item Added Successfully")#this not shoing in the template add leater okk
+        elif action == "update" and item_id:
+            item = Item.objects.get(id=item_id)
+            item.name = request.POST["name"]
+            item.quantity = request.POST["quantity"]
+            item.category = request.POST["category"]
+            item.price = request.POST["price"]
+            item.save()
+        elif action == "delete" and item_id:
+            Item.objects.filter(id=item_id).delete()
+
+        return redirect('Manage_items')
+
+    
+    items = Item.objects.all().order_by("-id")
+    edit_item = None
+    edit_id = request.GET.get("edit")
+    if edit_id:
+        try:
+            edit_item = Item.objects.get(id=edit_id)
+        except Item.DoesNotExist:
+            messages.error("item not found or dose not exist")
+    return render(request, 'Farmers/Manage_items.html', {"items": items, "edit_item": edit_item})
 
 def Farmers_logout(request):
     request.session.flush()
