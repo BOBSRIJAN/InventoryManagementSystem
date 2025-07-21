83eb416f773183637a5ecbfad4a6554d0241c547
SRIJAN_RAY <srijanray11@gmail.com>
2025-07-21 22:30:00 +0530

    UI design changes

--- Admin/urls.py
+++ Admin/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', Admin, name='custom_admin'),
     path("dashboard/", Dashboard, name="dashboard"),
+    path("user_management/", UserManagement, name="user_management"),
+    path('user/delete/<int:user_id>/', delete_user, name='delete_user'),
     path('admin/logout/', Admin_logout, name='admin_logout'),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib import messages
+from Farmers.models import Item as FarmerItem
+from Shopkeeper.models import Item as ShopkeeperItem
+from Auth.models import register
 
 # Create your views here.
 @login_required
@@ -10,6 +14,20 @@ def Admin(request):
 @login_required
 def Dashboard(request):
     return render(request, 'Admin/Dashboard.html')
+
+@login_required
+def UserManagement(request):
+    users = register.objects.all()
+    context = { 'users': users }
+    return render(request, 'Admin/UserManagement.html', context)
+
+@login_required
+def delete_user(request, user_id):
+    user = get_object_or_404(register, id=user_id)
+    if request.method == "POST":
+        user.delete()
+        messages.success(request, "User deleted.")
+    return redirect('user_management')
 
 def Admin_logout(request):
     logout(request)
