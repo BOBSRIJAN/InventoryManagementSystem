ec9d019850424aacbf18007d1e2a5f4f28a1dbf1
SRIJAN_RAY <srijanray11@gmail.com>
2025-07-27 21:39:50 +0530

    bug fix

--- User/views.py
+++ User/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
 from Auth.decorators import custom_login_required
 # Create your views here.
 @custom_login_required
 def user_dashboard(request):
-    return render(request, 'User/UserDashboard.html')
+    launch_date = datetime.now() + timedelta(days=7)
+    now = datetime.now()
+    time_left = launch_date - now
+
+    days = time_left.days
+    hours, remainder = divmod(time_left.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    context = {
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+    }
+    return render(request, 'User/UserDashboard.html', context)
